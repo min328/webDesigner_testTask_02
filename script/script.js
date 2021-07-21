9a937fcf1d6b21dcbd5e7e84a1ce8f2a661db15f
min328 <kmj20212021@gmail.com>
2021-07-21 15:41:56 +0900

    nav var, slide show function, done.

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
-/* eslint-disable */
+$(function(){
+
+    //headerGNB MENU
+    $("nav#headerGNB li.mainLi").mouseover(function(){
+        $("div#navBG").stop().slideDown(800);
+        $("nav#headerGNB ul.subMenu").stop().slideDown(800);
+    });
+
+    $("nav#headerGNB li.mainLi").mouseout(function(){
+        $("div#navBG").stop().slideUp(300);
+        $("nav#headerGNB ul.subMenu").stop().slideUp(300);
+    });
+
+    //Slideshow Function
+    setInterval(fn_slideShow, 3000);
+
+    function fn_slideShow() {
+        $("")
+        $("div#slideShuttle").animation(
+            { "margin-left" : "-1200px" }
+            , 1000
+            ,
+            $("#slideShuttle div:first-child").insertAfter("#slideShuttle div:last-child")
+        );
+
+    
+    }
+
+});
