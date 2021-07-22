3030a3b8a9af63ea5303ebd9e9888b2ded24eb2d
min328 <kmj20212021@gmail.com>
2021-07-22 15:31:49 +0900

    `slide show java script error solved

--- script/script.js
+++ script/script.js
@@ -1,12 +1,12 @@
 $(function(){
 
     //headerGNB MENU
-    $("nav#headerGNB li.mainLi a").mouseover(function(){
+    $("nav#headerGNB li.mainLi a, #navBG").mouseover(function(){
         $("div#navBG").stop().slideDown(800);
         $("nav#headerGNB ul.subMenu").stop().slideDown(800);
     });
 
-    $("nav#headerGNB li.mainLi a").mouseout(function(){
+    $("nav#headerGNB li.mainLi a, navBG").mouseout(function(){
         $("div#navBG").stop().slideUp(300);
         $("nav#headerGNB ul.subMenu").stop().slideUp(300);
     });
@@ -15,12 +15,16 @@ $(function(){
     setInterval(fn_slideShow, 3000);
 
     function fn_slideShow() {
-        $("")
-        $("div#slideShuttle").animation(
+        $("div#slideShuttle").animate(
             { "margin-left" : "-1200px" }
             , 1000
             ,
-            $("#slideShuttle div:first-child").insertAfter("#slideShuttle div:last-child")
+            function(){
+                $("#slideShuttle div:first-child").insertAfter("#slideShuttle div:last-child");
+                $("div#slideShuttle").css({
+                    "margin-left" : "0px"
+                });
+            }
         );
 
     
